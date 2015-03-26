57c73a9fe1779c05309f33cef735e0c580717e8e
Valar92 <jakub.skalecki@gmail.com>
2015-03-26 23:05:56 +0100

    added prototype of WebClient

---
+++ 2ndHandBookshop/WebClient/RegularBookshopWebClient.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using RestSharp;
+
+namespace WebClient
+{
+    public class RegularBookshopWebClient : IBookshopWebClient
+    {
+        private RestClient client;
+
+        public RegularBookshopWebClient(string baseEndpoint)
+        {
+            client = new RestClient(baseEndpoint);
+        }
+
+        public object GetUsers()
+        {
+            var request = new RestRequest("users", Method.GET);
+            var response = client.Execute(request);
+            Console.WriteLine(response.Content); // raw content as string
+            return response.Content;
+        }
+    }
+}
---
+++ 2ndHandBookshop/WebClientTests/TestClass.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using WebClient;
+
+namespace WebClientTests
+{
+    [TestClass]
+    public class TestClass
+    {
+        const string API_URL = "https://sheltered-forest-8633.herokuapp.com";
+        private IBookshopWebClient client = new RegularBookshopWebClient(API_URL);
+
+        [TestMethod]
+        public void TestGetUsers()
+        {
+            var users = client.GetUsers();
+            Assert.IsNotNull(users);
+        }
+    }
+}
