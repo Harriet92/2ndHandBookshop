6f4ac98819e15c19a0f2bf47ae9dde2f68c49c5d
Valian <jakub.skalecki@gmail.com>
2015-05-07 18:10:49 +0200

    Added basic logging

--- Server/app/app.py
+++ Server/app/app.py
@@ -1,4 +1,6 @@
+import logging
 from flask import Flask
+import sys
 
 from .conf import config
 
@@ -6,6 +8,7 @@ from .conf import config
 def create_app():
     application = Flask(__name__)
     application.config.from_object(config)
+    application.logger.setLevel(logging.DEBUG if application.config['DEBUG'] else logging.INFO)
     return application
 
 
